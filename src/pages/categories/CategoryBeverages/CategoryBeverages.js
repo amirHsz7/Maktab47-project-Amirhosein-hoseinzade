242d3e14e9af9c091a3a3e5b31d0d413fb4c9db8
amirHosein <amirhsz142@gmail.com>
2021-06-17 12:37:03 +0430

    add Cart & category beverage
    + Cart class
    + category beverage class

---
+++ src/pages/categories/CategoryBeverages/CategoryBeverages.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CategoryBeverages extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CategoryBeverages
