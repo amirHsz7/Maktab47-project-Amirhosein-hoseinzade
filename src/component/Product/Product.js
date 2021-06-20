382ecba26c0aafbec63c80c687f4545cde587ff7
amirHosein <amirhsz142@gmail.com>
2021-06-20 11:51:08 +0430

    create ProductComponent

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
-
+import Image from 'react-bootstrap/Image'
+import styles from './product.module.css'
 export class Product extends Component {
     render() {
         return (
-            <div>
-                
+            <div className={styles.container}>
+               <Image src={this.props.img} height='90' width='90' rounded />
+                <div className={styles.innercontainer}>
+                   <div className={styles.name}>{this.props.name}</div>
+                   <div className={styles.price}>{this.props.price} تومان</div>
+                </div>
             </div>
         )
     }
 }
 
-export default Product
+
